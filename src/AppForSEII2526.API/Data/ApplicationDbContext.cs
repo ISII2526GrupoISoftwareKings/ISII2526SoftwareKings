d25bb37679efb4a1540f420af89af778bd777745
Samuuell18 <samuelbruno.garcia@alu.uclm.es>
2025-10-01 23:04:10 +0200

    Task13 add applicationuser class to model folder (#21)
    ApplicationUser class modified.

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-
     public DbSet<PaymentMethod> PaymentMethods { get; set;}
     public DbSet<CreditCard> CreditCards { get; set; }
     public DbSet<Paypal> Paypals { get; set; }
@@ -19,4 +18,5 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Class> Classes { get; set; }
     public DbSet<PlanItem> PlanItems { get; set; }
     public DbSet<Plan> Plans { get; set; }
-    }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+}
--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,9 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    [StringLength(30, ErrorMessage = "Name cannot be longer than 30 characters.")]
+    public string Name { get; set; }
+
+    [StringLength(30, ErrorMessage = "Surname cannot be longer than 30 characters.")]
+    public string Surname { get; set; }
 }
