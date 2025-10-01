c0c0f735991a98623863da901dbdd361ef6211d1
Samuuell18 <samuelbruno.garcia@alu.uclm.es>
2025-10-01 21:06:23 +0200

    Task7 add class class to model folder (#15)
    Class Class created.

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Class> Classes { get; set; }
     public DbSet<PlanItem> PlanItems { get; set; }
     public DbSet<Plan> Plans { get; set; }
     }
