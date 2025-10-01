3c1460a71e0c336b498c8cf693c0e3198f701936
Samuuell18 <samuelbruno.garcia@alu.uclm.es>
2025-10-01 20:56:33 +0200

    PlanItem class created. (#14)

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using AppForSEII2526.API.Models;
 
 namespace AppForSEII2526.API.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) 
-{
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<PlanItem> PlanItems { get; set; }
     public DbSet<Plan> Plans { get; set; }
     }
