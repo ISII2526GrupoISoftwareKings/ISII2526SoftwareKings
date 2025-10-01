421dbf9272daa32afc7576883794346098f1f7c3
Samuuell18 <samuelbruno.garcia@alu.uclm.es>
2025-10-01 20:33:29 +0200

    Plan class created. (#2)

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -4,5 +4,7 @@ using AppForSEII2526.API.Models;
 
 namespace AppForSEII2526.API.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-}
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) 
+{
+    public DbSet<Plan> Plans { get; set; }
+    }
---
+++ src/AppForSEII2526.API/Models/Plan.cs
@@ -0,0 +1,33 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    [Index(nameof(Name), IsUnique = true)]
+    public class Plan
+    {
+        public int Id { get; set; }
+
+        [StringLength(20, ErrorMessage = "Name of Plan can be neither longer than 20 characters nor shorter than 1", MinimumLength = 1)]
+        public string Name { get; set; }
+
+        [StringLength(100, ErrorMessage = "Description cannot be longer than 100 characters.")]
+        public string? Description { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum quantity for renting is 1")]
+        public int Weeks { get; set; }
+
+        [DataType(DataType.Date), Display(Name = "Created Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+
+        [DataType(DataType.Currency),]
+        [Display(Name = "Total Price")]
+        [Precision(5, 2)]
+        public decimal TotalPrice { get; set; }
+
+        [Display(Name = "Health Issues")]
+        [StringLength(100, ErrorMessage = "Health Issues cannot be longer than 100 characters.")]
+        public string? HealthIssues { get; set; }
+    }
+}
