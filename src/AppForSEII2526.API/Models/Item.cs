6fcd9343f0bd7bb8cc83c74ea63ebda3153f648c
Hugo <rodriguezvillaloboshugo@gmail.com>
2025-10-02 18:21:26 +0200

    Task32 add item class to model folder

---
+++ src/AppForSEII2526.API/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
+        public string Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
+        public string Description { get; set; }
+
+        public decimal PurchasePrice { get; set; }
+
+        public int QuantityAvailableForPurchase { get; set; }
+
+        public int QuantityForRestock { get; set; }
+
+        public decimal? RestockPrice { get; set; }
+    }
+
+}
