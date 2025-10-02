a1a9fd44cf14d65beb464a5e8e494a26aee5c8f5
Hugo <rodriguezvillaloboshugo@gmail.com>
2025-10-03 00:00:03 +0200

    Added purchase item class to model folder

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -1,18 +1,22 @@
-﻿namespace AppForSEII2526.API.Models
+﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+
+namespace AppForSEII2526.API.Models
 {
     public class PurchaseItem
     {
         public int Id { get; set; }
 
+        [Display(Name = "Amount Bought")]
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum amount bought is 1")]
         public int AmountBought { get; set; }
 
+        [DataType(DataType.Currency)]
+        [Precision(10, 2)]
         public decimal Price { get; set; }
 
-        // Relación con Item
         public int ItemId { get; set; }
         public Item Item { get; set; }
 
-        // Relación con Purchase
         public int PurchaseId { get; set; }
         public Purchase Purchase { get; set; }
     }
