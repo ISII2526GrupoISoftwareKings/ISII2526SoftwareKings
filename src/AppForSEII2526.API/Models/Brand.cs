199dec99bfc1bcde8c4a6a7e91b6d5228d8f7c31
AlbertoBueno18 <alberto.bueno@mylabs.es>
2025-10-02 20:56:08 +0200

    Brand and Item classes created

---
+++ src/AppForSEII2526.API/Models/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Item> Items { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal RestockPrice { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public int QuantityForRestock { get; set; }
+        public int QuantityAvailableForPurchase { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
