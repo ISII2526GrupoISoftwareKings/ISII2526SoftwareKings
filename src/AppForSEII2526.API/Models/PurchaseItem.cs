dbfa528bb1b9f8d5c5c312b5e8b625207258407b
Hugo <rodriguezvillaloboshugo@gmail.com>
2025-10-02 23:36:57 +0200

    Added purchase item class to model folder

---
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -0,0 +1,19 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PurchaseItem
+    {
+        public int Id { get; set; }
+
+        public int AmountBought { get; set; }
+
+        public decimal Price { get; set; }
+
+        // Relación con Item
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+
+        // Relación con Purchase
+        public int PurchaseId { get; set; }
+        public Purchase Purchase { get; set; }
+    }
+}
