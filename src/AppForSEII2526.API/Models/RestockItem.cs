a9b62b487460178fffa9d39cc5197183ee9ab4bc
AlbertoBueno18 <alberto.bueno@mylabs.es>
2025-10-03 18:11:47 +0200

    ApplicationDbContext implemented for restock and compound key created in RestockItem

--- src/AppForSEII2526.API/Models/RestockItem.cs
+++ src/AppForSEII2526.API/Models/RestockItem.cs
@@ -1,5 +1,8 @@
-﻿namespace AppForSEII2526.API.Models
+﻿using System.Numerics;
+
+namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(ItemId), nameof(RestockId))]
     public class RestockItem
     {
         public int ItemId { get; set; }
