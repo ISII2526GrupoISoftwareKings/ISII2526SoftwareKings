a766c8fa7384e4017f083ff2ba2783b0c1c62a4b
HugoRodriguezUCLM <hugo.rodriguez@alu.uclm.es>
2025-10-03 13:28:22 +0200

    Completed Item class in model folder (#46)
    Co-authored-by: Hugo <rodriguezvillaloboshugo@gmail.com>

--- src/AppForSEII2526.API/Models/Item.cs
+++ src/AppForSEII2526.API/Models/Item.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.CodeAnalysis.Elfie.Model.Tree;
 using System.Drawing.Drawing2D;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DataType = System.ComponentModel.DataAnnotations.DataType;
 
 namespace AppForSEII2526.API.Models
 {
@@ -11,14 +14,22 @@ namespace AppForSEII2526.API.Models
         public string Name { get; set; }
 
         [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
+        [DataType(DataType.Currency)]
+        [Precision(10, 2)]
         public decimal PurchasePrice { get; set; }
 
+        [Display(Name = "Quantity Available For Purchase")]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity available must be 0 or more")]
         public int QuantityAvailableForPurchase { get; set; }
 
+        [Display(Name = "Quantity For Restock")]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity for restock must be 0 or more")]
         public int QuantityForRestock { get; set; }
 
+        [DataType(DataType.Currency)]
+        [Precision(10, 2)]
         public decimal? RestockPrice { get; set; }
         public List<PurchaseItem> PurchaseItems { get; set; }
         public TypeItem TypeItem { get; set; }
