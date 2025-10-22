bd87ba38e6cd2f3989c77068cd8c634fdb00eaff
SAMUEL BRUNO GARCÍA PICAZO <SamuelBruno.Garcia@alu.uclm.es>
2025-10-22 21:57:45 +0200

    Controller and DTOs created for GetPlan Task

---
+++ src/AppForSEII2526.API/Controllers/PlansController.cs
@@ -0,0 +1,52 @@
+﻿using AppForSEII2526.API.DTOs.PlanDTOs;
+using AppForSEII2526.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlansController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<PlansController> _logger;
+
+        public PlansController(ApplicationDbContext context, ILogger<PlansController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(PlanForDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetPlan(int id)
+        {
+            PlanForDetailDTO? plan = await _context.Plans
+                .Where(p => p.Id == id)
+                .Include(p => p.PlanItems)
+                    .ThenInclude(pi => pi.Class)
+                        .ThenInclude(c => c.TypeItems)
+
+                .Include(p => p.PaymentMethod)
+                    .ThenInclude(pm => pm.User)
+
+                .Select(p => new PlanForDetailDTO(p.Id, p.TotalPrice, p.CreatedDate, p.Name, p.PaymentMethod.User.Name, p.PaymentMethod.User.Surname, p.Description, p.Weeks, p.CreatedDate, p.HealthIssues, p.PaymentMethod, p.PlanItems
+                    .Select(pi => new PlanItemDTO(pi.PlanId, pi.Class.Name, pi.Class.Price, pi.Class.Capacity, pi.Class.Date, pi.Goal)).ToList())).FirstOrDefaultAsync();
+
+
+            if (plan == null)
+            {
+                _logger.LogError($"Plan with id {id} not found.", id);
+                return NotFound();
+            }
+
+            return Ok(plan);
+        }
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/PlanForDetailDTO.cs
@@ -0,0 +1,22 @@
+﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class PlanForDetailDTO:PlanForCreateDTO
+    {
+        public PlanForDetailDTO(int id, decimal totalPrice, DateTime planDate, string name, string nameUser, string surnameUser, string? description, int weeks, DateTime createdDate, string? healthIssues, PaymentMethod paymentMethod, List<PlanItemDTO> planItems) : base(name, nameUser, surnameUser, description, weeks, createdDate, healthIssues, paymentMethod, planItems)
+        {
+            Id = id;
+            TotalPrice = totalPrice;
+            PlanDate = planDate;
+        }
+
+        public int Id { get; set; }
+
+        [DataType(DataType.Currency),]
+        [Display(Name = "Total Price")]
+        [Precision(5, 2)]
+        public decimal TotalPrice { get; set; }
+
+        public DateTime PlanDate { get; set; }
+    }
+}
