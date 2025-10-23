2e15cbcf0a0083aa2f1a814ca1e6ea238aebb63c
SAMUEL BRUNO GARCÍA PICAZO <SamuelBruno.Garcia@alu.uclm.es>
2025-10-23 10:44:25 +0200

    Creating the structure to develop de Unit Testing.

---
+++ test/AppForSEII2526.UT/ClassesController_test/SelectClassesForPlan_test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UT.ClassesController_test
+{
+    internal class SelectClassesForPlan_test
+    {
+    }
+}
