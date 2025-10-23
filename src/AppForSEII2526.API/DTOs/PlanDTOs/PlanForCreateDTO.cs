e6c2405c2400551e1e38f7bfa4abb6b2d2d94f55
Samuuell18 <samuelbruno.garcia@alu.uclm.es>
2025-10-23 08:29:36 +0200

    Controller and DTOs created for GetPlan Task (#68)

---
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/PlanForCreateDTO.cs
@@ -0,0 +1,50 @@
+﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+
+
+namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class PlanForCreateDTO
+    {
+        public PlanForCreateDTO(string name, string nameUser, string surnameUser, string? description, int weeks, DateTime createdDate, string? healthIssues, PaymentMethod paymentMethod, List<PlanItem> planItems)
+        {
+            Name = name;
+            NameUser = nameUser;
+            SurnameUser = surnameUser;
+            Description = description;
+            Weeks = weeks;
+            CreatedDate = createdDate;
+            HealthIssues = healthIssues;
+            PaymentMethod = paymentMethod;
+            PlanItems = planItems;
+        }
+
+        [StringLength(20, ErrorMessage = "Name of Plan can be neither longer than 20 characters nor shorter than 1", MinimumLength = 1)]
+        public string Name { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must have at least 3 characters")]
+        public string NameUser { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Surname must have at least 3 characters")]
+        public string SurnameUser { get; set; }
+
+        [StringLength(100, ErrorMessage = "Description cannot be longer than 100 characters.")]
+        public string? Description { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum quantity for renting is 1")]
+        public int Weeks { get; set; }
+
+        [DataType(DataType.Date), Display(Name = "Created Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+
+        
+
+        [Display(Name = "Health Issues")]
+        [StringLength(100, ErrorMessage = "Health Issues cannot be longer than 100 characters.")]
+        public string? HealthIssues { get; set; }
+
+
+        public PaymentMethod PaymentMethod { get; set; }
+        public List<PlanItem> PlanItems { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/PlanItemDTO.cs
@@ -0,0 +1,40 @@
+﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+
+
+namespace AppForSEII2526.API.DTOs.PlanDTOs
+
+{
+    public class PlanItemDTO
+    {
+        public PlanItemDTO(int classId, string name, decimal price, int capacity, DateTime date, string goal)
+        {
+            ClassId = classId;
+            Name = name;
+            Price = price;
+            Capacity = capacity;
+            Date = date;
+            Goal = goal;
+        }
+
+        public int ClassId { get; set; }
+
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Precision(5, 2)]
+        public decimal Price { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum capacity is 1.")]
+        public int Capacity { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        [StringLength(100, ErrorMessage = "Goal can be neither longer than 100 characters nor shorter than 1", MinimumLength = 1)]
+        public string Goal { get; set; }
+
+        public List<TypeItem> TypeItems { get; set; }
+    }
+}
