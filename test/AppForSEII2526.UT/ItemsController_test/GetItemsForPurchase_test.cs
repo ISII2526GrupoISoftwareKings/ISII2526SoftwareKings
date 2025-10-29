83b038a56e35e30cc30b7c1fd6642a6962d7b63f
Hugo <rodriguezvillaloboshugo@gmail.com>
2025-10-29 10:11:38 +0100

    Starting unit testing, folders and getitemsforpurchase_test created

---
+++ test/AppForSEII2526.UT/ItemsController_test/GetItemsForPurchase_test.cs
@@ -0,0 +1,19 @@
+﻿using AppForMovies.UT;
+using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.Data;          
+using AppForSEII2526.API.Models;       
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppForSEII2526.UT.ItemsController_test
+{
+    internal class GetItemsForPurchase_test : AppForSEII25264SqliteUT
+    {
+    }
+}
