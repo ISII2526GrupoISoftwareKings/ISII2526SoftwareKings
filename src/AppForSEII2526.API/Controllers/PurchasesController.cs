0bbc166a605566677a3e9b3d2ed7e8e15ec81bd6
Hugo <rodriguezvillaloboshugo@gmail.com>
2025-10-29 10:55:36 +0100

    Post created

--- src/AppForSEII2526.API/Controllers/PurchasesController.cs
+++ src/AppForSEII2526.API/Controllers/PurchasesController.cs
@@ -84,9 +84,99 @@ namespace AppForSEII2526.API.Controllers
         }
         [HttpPost]
         [Route("[action]")]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+        [ProducesResponseType(typeof(PurchaseForDetailDTO), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult> CreatePurchase(PurchaseForCreateDTO purchaseForCreate)
+        {
+            //any validation defined in PurchaseForCreateDTO is checked before running the method so they don't have to be checked again
+            if (purchaseForCreate.PaymentMethod == null)
+                ModelState.AddModelError("PaymentMethod", "Error! You must select a payment method");
 
-        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.Created)]
+            if (string.IsNullOrEmpty(purchaseForCreate.City) || string.IsNullOrEmpty(purchaseForCreate.Country) || string.IsNullOrEmpty(purchaseForCreate.Street))
+                ModelState.AddModelError("Address", "Error! City, Country and Street are mandatory");
 
-        [ProducesResponseType(typeof(PurchaseForDetailDTO), (int)HttpStatusCode.BadRequest)]
+            if (purchaseForCreate.PurchaseItems.Count == 0)
+                ModelState.AddModelError("PurchaseItems", "Error! You must include at least one item to purchase");
+
+            //we must check that all the items to be purchased exist in the database
+            var itemIds = purchaseForCreate.PurchaseItems.Select(pi => pi.ItemId).ToList<int>();
+
+            var items = await _context.Items
+                .Where(i => itemIds.Contains(i.Id))
+                .ToListAsync();
+
+            //we must provide purchase with the info to be saved in the database
+            Purchase purchase = new Purchase(
+                purchaseForCreate.City,
+                purchaseForCreate.Country,
+                DateTime.Now,
+                purchaseForCreate.Street,
+                purchaseForCreate.TotalPrice,
+                purchaseForCreate.PaymentMethod,
+                purchaseForCreate.Description
+            );
+
+            foreach (var pi in purchaseForCreate.PurchaseItems)
+            {
+                var item = items.FirstOrDefault(i => i.Id == pi.ItemId);
+
+                //we must check that there is enough quantity to be purchased in the database
+                if (item == null)
+                {
+                    ModelState.AddModelError("PurchaseItems", $"Error! Item with id '{pi.ItemId}' does not exist");
+                }
+                else if (pi.AmountBought > item.QuantityAvailableForPurchase)
+                {
+                    ModelState.AddModelError("PurchaseItems", $"Error! Requested quantity for item '{item.Name}' exceeds available stock");
+                }
+                else
+                {
+                    // purchase does not exist in the database yet, so we must relate purchaseitem to the object purchase
+                    purchase.PurchaseItems.Add(new PurchaseItem
+                    {
+                        ItemId = item.Id,
+                        Item = item,
+                        AmountBought = pi.AmountBought,
+                        Price = pi.Price
+                    });
+                    pi.Price = item.PurchasePrice;
+                }
+            }
+
+            //if there is any problem because of the available quantity of items or because any item does not exist
+            if (ModelState.ErrorCount > 0)
+            {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            _context.Add(purchase);
+
+            try
+            {
+                //we store in the database both purchase and its purchaseitems
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(DateTime.Now + ":" + ex.Message);
+                return Conflict("Error: " + ex.Message);
+            }
+
+            //prepare the DTO to return to the user
+            var purchaseDetail = new PurchaseForDetailDTO(
+                purchase.Id,
+                purchase.TotalPrice,
+                purchase.City,
+                purchase.Country,
+                purchase.Street,
+                purchase.Description,
+                purchase.Date,
+                purchase.PaymentMethod,
+                purchaseForCreate.PurchaseItems
+            );
+
+            return CreatedAtAction("GetPurchase", new { id = purchase.Id }, purchaseDetail);
+        }
     }
 }
