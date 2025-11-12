406a92c3e8b0ef3412e4facd000b079ed7fefe82
SAMUEL BRUNO GARCÍA PICAZO <SamuelBruno.Garcia@alu.uclm.es>
2025-11-12 12:56:40 +0100

    Fixing issues.

--- src/AppForSEII2526.API/Controllers/ClassesController.cs
+++ src/AppForSEII2526.API/Controllers/ClassesController.cs
@@ -1,4 +1,5 @@
 ﻿using AppForSEII2526.API.DTOs.ClassDTOs;
+using AppForSEII2526.API.DTOs.TypeItemDTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -46,9 +47,6 @@ namespace AppForSEII2526.API.Controllers
 
             if (date != null && date < startDate)
             {
-                //return BadRequest( Problem("fromDate must be earlier than toDate", 
-                //    $"fromDate ({fromDate}) toDate({toDate})", 400,"Bad Request", 
-                //    "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1"));
                 ModelState.AddModelError("Date&finalDate", "Date must be later than 10/10/2025");
                 _logger.LogError($"{DateTime.Now} Error: Date must be later than 10/10/2025");
                 return BadRequest(new ValidationProblemDetails(ModelState));
@@ -63,11 +61,11 @@ namespace AppForSEII2526.API.Controllers
 
 
 
-            IList<ClassForPlanDTO> classesDTO = await _context.Classes
+            IList<ClassForPlanDTO> classesDTO = await _context.Classes 
                 .Include(c => c.TypeItems)
                 .Where(c => (c.Name.Contains(className) || (className == null)) && c.Date >= date && c.Date <= finalDate)
                 .OrderBy(c => c.Id)
-                .Select(c => new ClassForPlanDTO(c.Id, c.Name, c.TypeItems, c.Date, c.Price))
+                .Select(c => new ClassForPlanDTO(c.Id, c.Name, c.TypeItems.Select(ti => new TypeItemForClassDTO(ti.Id, ti.Name)).ToList(), c.Date, c.Price))
                 .ToListAsync();
 
             if (!classesDTO.Any())
---
+++ src/AppForSEII2526.API/DTOs/TypeItemDTOs/TypeItemForClassDTO.cs
@@ -0,0 +1,27 @@
+﻿
+namespace AppForSEII2526.API.DTOs.TypeItemDTOs
+{
+    public class TypeItemForClassDTO
+    {
+        public TypeItemForClassDTO(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TypeItemForClassDTO dTO &&
+                   Id == dTO.Id &&
+                   Name == dTO.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
+    }
+}
