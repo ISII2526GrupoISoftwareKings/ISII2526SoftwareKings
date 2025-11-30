a362356d49af7028a2064df561f5973f8e25b01a
Hugo Rodríguez Villalobos <hugo.rodriguez@alu.uclm.es>
2025-11-30 21:00:11 +0100

    Task131 develop UI for selecting items for purchase (#132)
    * Add PurchaseStateContainer class
    
    * Fixing errors with PurchaseStateContainer
    
    * Add Purchase page and update navigation menu
    
    * add GetBrands API endpoint to swagger
    
    * Update swagger with GetBrands API endpoint
    
    * Update navigation menu icon
    
    ---------
    
    Co-authored-by: Hugo <rodriguezvillaloboshugo@gmail.com>

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -5,6 +5,7 @@ using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
 using AppForSEII2526.Web.API;
+using AppForSEII2526.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,6 +42,9 @@ string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForSEII2526
 
 //we create the service for accessing the API from where .WEB project
 builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIClient(URI2API, new HttpClient()));
+
+
+builder.Services.AddScoped<PurchaseStateContainer>();
 
 var app = builder.Build();
 
