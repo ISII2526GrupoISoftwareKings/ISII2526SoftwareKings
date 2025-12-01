b0e4eff61a9014a4868c05ad7214cea0d3d67105
SAMUEL BRUNO GARCÍA PICAZO <SamuelBruno.Garcia@alu.uclm.es>
2025-12-01 17:11:05 +0100

    Type Items added.

--- src/AppForSEII2526.API/Data/dbo.Classes.data.sql
+++ src/AppForSEII2526.API/Data/dbo.Classes.data.sql
@@ -1,8 +1,8 @@
 SET IDENTITY_INSERT [dbo].[Classes] ON
-INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (6, N'Yoga', CAST(11.00 AS Decimal(5, 2)), 20, N'2026-01-10 00:00:00')
-INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (10, N'Spinning', CAST(11.00 AS Decimal(5, 2)), 20, N'2026-01-11 00:00:00')
-INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (12, N'CrossFit', CAST(10.00 AS Decimal(5, 2)), 20, N'2026-01-12 00:00:00')
-INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (13, N'Strech & Relax', CAST(10.00 AS Decimal(5, 2)), 25, N'2026-01-13 00:00:00')
-INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (14, N'Zumba', CAST(8.00 AS Decimal(5, 2)), 20, N'2026-01-14 00:00:00')
-INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (15, N'Pilates', CAST(10.00 AS Decimal(5, 2)), 25, N'2026-01-15 00:00:00')
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (6, N'Yoga', CAST(11.00 AS Decimal(5, 2)), 20, N'2026-01-10 18:00:00')
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (10, N'Spinning', CAST(11.00 AS Decimal(5, 2)), 20, N'2026-01-11 17:00:00')
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (12, N'CrossFit', CAST(10.00 AS Decimal(5, 2)), 20, N'2026-01-12 16:00:00')
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (13, N'Strech & Relax', CAST(10.00 AS Decimal(5, 2)), 25, N'2026-01-13 20:00:00')
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (14, N'Zumba', CAST(8.00 AS Decimal(5, 2)), 20, N'2026-01-14 17:30:00')
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Price], [Capacity], [Date]) VALUES (15, N'Pilates', CAST(10.00 AS Decimal(5, 2)), 25, N'2026-01-15 15:30:00')
 SET IDENTITY_INSERT [dbo].[Classes] OFF
