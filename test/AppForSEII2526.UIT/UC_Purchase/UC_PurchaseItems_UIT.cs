cf6d4f32a0b0f2574ce891c45ccdb76839b2c848
Hugo <hugo.rodriguez@alu.uclm.es>
2025-12-03 10:36:41 +0100

    Creating folder and class for UIT

---
+++ test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseItems_UIT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Purchase
+{
+    public class UC_PurchaseItems_UIT : UC_UIT
+    {
+        public UC_PurchaseItems_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
