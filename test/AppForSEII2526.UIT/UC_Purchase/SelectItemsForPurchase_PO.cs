378bf22053c4cb313a0565ebc1aa139ca1a9c22d
Hugo <hugo.rodriguez@alu.uclm.es>
2025-12-03 14:20:12 +0100

    we create the class for implementing the UIT test cases

--- test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_PO.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_PO.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppForSEII2526.UIT.UC_Purchase
-{
-    public class SelectItemsForPurchase_PO : PageObject
-    {
-        public SelectItemsForPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
-        {
+namespace AppForSEII2526.UIT.UC_Purchase {
+    public class SelectItemsForPurchase_PO : PageObject {
+
+        By inputItemName = By.Id("inputItemName");
+        By inputBrandName = By.Id("inputBrandName");
+        By buttonSearchItems = By.Id("searchItems");
+        By tableOfItemsBy = By.Id("TableOfItems");
+
+        public SelectItemsForPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) {
+        }
+
+        public void SearchItems(string itemName, string itemBrand) {
+            //wait for the webelement to be clickable
+            WaitForBeingClickable(inputItemName);
+            _driver.FindElement(inputItemName).SendKeys(itemName);
+
+            if (itemBrand != "")
+                _driver.FindElement(inputBrandName).SendKeys(itemBrand);
+
+            _driver.FindElement(buttonSearchItems).Click();
+        }
+
+        public bool CheckListOfItems(List<string[]> expectedItems) {
+            return CheckBodyTable(expectedItems, tableOfItemsBy);
         }
     }
 }
--- test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseItems_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseItems_UIT.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppForSEII2526.UIT.UC_Purchase
-{
-    public class UC_PurchaseItems_UIT : UC_UIT
-    {
-        public UC_PurchaseItems_UIT(ITestOutputHelper output) : base(output)
-        {
+namespace AppForSEII2526.UIT.UC_Purchase {
+    public class UC_PurchaseItems_UIT : UC_UIT {
+
+        private SelectItemsForPurchase_PO selectItemsForPurchase_PO;
+
+        public UC_PurchaseItems_UIT(ITestOutputHelper output) : base(output) {
+        }
+
+        private void Precondition_perform_login() {
+            Perform_login("wedonthaveone@gmail.com", "wedonthaveone");
+        }
+
+        private void InitialStepsForPurchaseItems() {
+            Precondition_perform_login();
+            //we wait for the option of the menu to be visible
+            selectItemsForPurchase_PO.WaitForBeingVisible(By.LinkText("Purchase"));
+            //we click on the menu
+            _driver.FindElement(By.LinkText("Purchase")).Click();
         }
     }
 }
