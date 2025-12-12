31eca6bbbbfbef58ff369c6a1841413ad1c9faf4
AlbertoBueno18 <alberto.bueno@mylabs.es>
2025-12-12 22:20:21 +0100

    Component for creating a restock created

--- src/AppForSEII2526.Web/RestockStateContainer.cs
+++ src/AppForSEII2526.Web/RestockStateContainer.cs
@@ -4,18 +4,15 @@ namespace AppForSEII2526.Web
 {
     public class RestockStateContainer
     {
-        // We create an instance of RestockForCreateDTO when an instance of RestockStateContainer is created
         public RestockForCreateDTO Restock { get; private set; } = new RestockForCreateDTO()
         {
             RestockItems = new List<RestockItemDTO>()
         };
 
-        // We compute the TotalPrice of the items we have selected for the restock
         public decimal TotalPrice
         {
             get
             {
-                // Sum of (Price * Quantity to buy). We handle null prices with ?? 0
                 return (decimal) Restock.RestockItems.Sum(i => (i.PriceOfRestock ?? 0) * i.QuantityForRestock);
             }
         }
@@ -26,39 +23,33 @@ namespace AppForSEII2526.Web
 
         public void AddItemToRestock(ItemForRestockDTO itemDto)
         {
-            // Before adding an item we check whether it has been already added to avoid duplicates
             if (!Restock.RestockItems.Any(ri => ri.ItemId == itemDto.Id))
             {
-                // We add it if it is not in the list
-                // Note: We set a default QuantityForRestock (amount to buy) to 1
+
                 Restock.RestockItems.Add(new RestockItemDTO()
                 {
                     ItemId = itemDto.Id,
                     Name = itemDto.Name,
                     Brand = itemDto.Brand,
-                    QuantityInRestock = itemDto.QuantityForRestock, // Current stock level from DB
+                    QuantityInRestock = itemDto.QuantityForRestock,
                     PriceOfRestock = itemDto.RestockPrice,
-                    QuantityForRestock = 1 // Default purchase amount
+                    QuantityForRestock = 1
                 });
             }
         }
 
-        // To delete items from the list of selected restock items
         public void RemoveItemFromRestock(RestockItemDTO item)
         {
             Restock.RestockItems.Remove(item);
         }
 
-        // We eliminate all the items from the list
         public void ClearRestockCart()
         {
             Restock.RestockItems.Clear();
         }
 
-        // We have already finished the process of restocking, thus, we create a new Restock object
         public void RestockProcessed()
         {
-            // We have finished the restock process so we create a new object without data
             Restock = new RestockForCreateDTO()
             {
                 RestockItems = new List<RestockItemDTO>()
